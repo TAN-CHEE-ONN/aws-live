3ac8eda6a96cc3a1b4f8feca180f41fa44d4f91c
TAN-CHEE-ONN <77651630+TAN-CHEE-ONN@users.noreply.github.com>
2022-03-18 12:00:56 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cenumlmfsqnw.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tancheeonn-employee"
+customregion = "us-east-1"
